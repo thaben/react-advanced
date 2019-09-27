8307f5589dd6f238f8d0f1f3e222f9050c8a8d57
Thanasis <athbenos@gmail.com>
2019-09-27 13:24:08 +0300

    HoC solution

---
+++ app/examples/HoC/solution/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import withUser from "./withUser";
+
+const Card = ({ title, user }) => (
+  <div>
+    <h2 className="is-size-2">{title}</h2>
+    {user && (
+      <div className="card">
+        <img src={user.imgPath} className="card-img-top" alt={`${user.name} photo.`} />
+        <div className="card-body">
+          <h5 className="card-title">{user.name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">JWT: {user.JWT}</h6>
+          <p className="card-text">
+            {user.username} | is admin: {user.admin ? "yes" : "no"}
+          </p>
+        </div>
+      </div>
+    )}
+  </div>
+);
+
+export default withUser(Card);
